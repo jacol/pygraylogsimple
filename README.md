d77ff4ac36ebf356f60597e7b156d5c62f2aacf6
jacek <jacek>
2018-01-29 17:56:32 +0100

    Add parametrized query, limit and offset

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pygraylogsimple
 Simple graylog lib that use offsets and limits
+
+##Usage
+'''python
+
+#initialize class with username, password and host
+api = pygraylogsimple.PyGraylogSimple('user', 'password', 'http://host:port')
+
+#use fuctions to access particular Graylog API method - last 2 params: offset and limit are optional
+data = api.search_universal_absolute('*', '2017-07-01T00:00:00.000Z', '2017-07-02T00:00:00.000Z', 0, 100)
+
+'''
--- pygraylogsimple.py
+++ pygraylogsimple.py
@@ -9,14 +9,38 @@ class PyGraylogSimple:
         self.password = password
         self.host = host
 
-    def getdata(self, querystring):
+    def build_search_universal_absolute_url(self, query, date_time_from, date_time_to):
+        """
+        :param query: use * for everything
+        :param date_time_from: format - 2017-07-01T00:00:00.000Z
+        :param date_time_to: format - 2017-07-01T00:00:00.000Z
+        :return: complete url to Graylog API
+        """
+        return '{0}/search/universal/absolute?query={1}&from={2}&to={3}'. \
+            format(self.host, query, date_time_from, date_time_to)
+
+    def run_query(self, url, offset=None, limit=None):
+        """
+        :param url: complete url to call
+        :param offset: optional
+        :param limit: optional
+        :return: response from Graylog API as string
+        """
+
         response = StringIO.StringIO()
         c = pycurl.Curl()
 
-        c.setopt(c.URL, self.host + querystring)
+        if offset is not None:
+            url += '&offset=' + str(offset)
+
+        if limit is not None:
+            url += '&limit=' + str(limit)
+
+        c.setopt(c.URL, url)
         c.setopt(c.WRITEFUNCTION, response.write)
         c.setopt(c.HTTPHEADER, ['Content-Type: application/json', 'Accept-Charset: UTF-8'])
         c.setopt(c.USERPWD, self.user + ':' + self.password)
+        c.setopt(c.HEADERFUNCTION, response.write)
         c.perform()
         c.close()
 
